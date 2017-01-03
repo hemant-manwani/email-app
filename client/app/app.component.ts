6a571a5ff3c4b2cac30f5931555642adb93b89d7
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-04 01:45:55 +0530

    HTML for email-app

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: [ 'app.component.css' ]
 })
-export class AppComponent  {
-  
+export class AppComponent implements OnInit  {
+  ngOnInit() {
+    document.getElementById("wrapper").style.minHeight = (window.innerHeight - document.getElementsByTagName("header")[0].offsetHeight - document.getElementsByTagName("footer")[0].offsetHeight)+'px'
+  }
 }
